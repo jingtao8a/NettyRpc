97afa23f0aa92f3c8609fb4bb60791bbb59b9a22
yuxintao <1921056015@qq.com>
2024-01-16 16:03:28 +0800

    添加RpcException，修改RpcResponseErrorMsgEnum

---
+++ rpc-common/src/main/java/org/jingtao8a/exception/RpcException.java
@@ -0,0 +1,9 @@
+package org.jingtao8a.exception;
+
+import org.jingtao8a.consts.enums.RpcResponseErrorMsgEnum;
+
+public class RpcException extends RuntimeException{
+    public RpcException(RpcResponseErrorMsgEnum rpcResponseErrorMsgEnum, String detail) {
+        super(rpcResponseErrorMsgEnum.getMessage() + ":" + detail);
+    }
+}
