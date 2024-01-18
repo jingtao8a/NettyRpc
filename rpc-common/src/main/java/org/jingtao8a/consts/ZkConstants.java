4d4c626412b1067a2fe202a06e50bd65747ab52c
yuxintao <1921056015@qq.com>
2024-01-18 11:35:15 +0800

    删除CuratorClient和ZkConstants

--- rpc-common/src/main/java/org/jingtao8a/consts/ZkConstants.java
+++
@@ -1,9 +0,0 @@
-package org.jingtao8a.consts;
-
-public class ZkConstants {
-    public static int ZK_SESSION_TIMEOUT = 5000;
-    public static int ZK_CONNECTION_TIMEOUT = 5000;
-    public static String ZK_REGISTRY_PATH = "/registry";
-    public static String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
-    public static String ZK_NAMESPACE = "nettyrpc";
-}
