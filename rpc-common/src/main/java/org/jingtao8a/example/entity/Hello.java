85adae005bcef8540031ec84893b1680b5b08efa
yuxintao <1921056015@qq.com>
2024-01-19 20:17:42 +0800

    Rpc调用，传送的自定义对象需要实现Serializable接口，并且kryo序列化需要无参构造函数

--- rpc-common/src/main/java/org/jingtao8a/example/entity/Hello.java
+++ rpc-common/src/main/java/org/jingtao8a/example/entity/Hello.java
@@ -2,6 +2,7 @@ package org.jingtao8a.example.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Hello implements Serializable {
     private String message;
     private String destription;
