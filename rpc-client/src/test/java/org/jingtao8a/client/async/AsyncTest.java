0050ae302faf0491742c904daa56d89b74ec6ed0
yuxintao <1921056015@qq.com>
2024-01-19 16:12:09 +0800

    测试async模块

---
+++ rpc-client/src/test/java/org/jingtao8a/client/async/AsyncTest.java
@@ -0,0 +1,41 @@
+package org.jingtao8a.client.async;
+
+import org.jingtao8a.client.core.PendingRpcRequests;
+import org.jingtao8a.consts.enums.RpcResponseCodeEnum;
+import org.jingtao8a.dto.RpcResponse;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+public class AsyncTest {
+    @Test
+    public void test1() {
+        RpcContext.setCallback(new CustomResponseCallback());
+        String requestId = "1234";
+        RpcFuture rpcFuture = new RpcFuture();
+        rpcFuture.setResponseCallback(RpcContext.getCallback());
+
+        PendingRpcRequests.put(requestId, rpcFuture);
+        PendingRpcRequests.complete(new RpcResponse(requestId, RpcResponseCodeEnum.SUCCESS.getCode(), RpcResponseCodeEnum.SUCCESS.getMessage(), "fkdjfkas"));
+        try {
+            Thread.sleep(2);
+            System.out.println("get response" + rpcFuture.get().toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static class CustomResponseCallback extends ResponseCallback {
+
+        @Override
+        public void callBack(RpcResponse result) {
+            System.out.println("callBack");
+            System.out.println(result.toString());
+        }
+
+        @Override
+        public void onException(RpcResponse result, Exception e) {
+
+        }
+    }
+}
